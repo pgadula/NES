d5bcc42b7c30274406c82a64a4b2b77d6b1be80d
pgadula <pgadula@Mac-mini-pgadula.local>
2025-05-20 19:49:17 +0200

    add adressing modes

--- src/m6502.rs
+++ src/m6502.rs
@@ -1,20 +1,16 @@
 
 use std::fmt::{self, write, Display, Formatter};
+use regex::Regex;
 
 use bitflags::bitflags;
 
-bitflags! {
-    #[derive(Debug)]
-    pub struct PFlag: u32 {
-        const Carry =            0b00000001;
-        const Zero =             0b00000010;
-        const InterruptDisable = 0b00000100;
-        const DecimalMode =      0b00001000;
-        const BreakCommand =     0b00010000;
-        const Overflow =         0b00100000;
-        const Negative =         0b01000000;
-    }
-}
+use crate::opcodes::OPCODES_STRING;
+
+pub const STACK_BASE: u8 = 0x01;
+pub const VECTOR_BASE: u8 = 0xFF;
+pub const IRQ_BRK_VECTOR: u8 = 0xFE;
+pub const RESET_VECTOR: u8 = 0xFC;
+pub const NMI_VECTOR: u8 = 0xFA;
 
 #[derive(Debug)]
 pub struct Mos6502{
@@ -25,7 +21,6 @@ pub struct Mos6502{
     pub x: u8,
     pub y: u8,
     pub sp: u8,
-
 }
 
 #[derive(Debug)]
@@ -34,20 +29,40 @@ pub struct Bus{
 }
 
 impl Bus {
-    pub fn new()->Bus{
-       return Bus{
-           memory: [0; 2048] 
-       } 
-    }
+        pub fn new()->Bus{
+        return Bus{
+            memory: [0; 2048] 
+        } 
+        }
+        pub fn dump(&self){
+            println!("{}", self);
+        }
+        
+        pub fn read_u16(&mut self, hi:u8, le:u8)->u8{
+        let mut adr: u16 = le as u16;
+        adr |= (hi as u16) << 8;
+        println!("{:04X}", adr);
+        self.read(adr) 
+        }
 
-    pub fn read(& self, address: usize)->u8{
-        return self.memory[address];
-    }
+        pub fn read(& self, address: u16)->u8{
+            return self.memory[address as usize];
+        }
 
-    pub fn write(& mut self, address: usize, value: u8){
-        self.memory[address] = value;
-    }
+        pub fn write(&mut self, address: usize, value: u8){
+            self.memory[address] = value;
+        }
+
+        
+        pub fn write_bytes(&mut self, address: usize, value: &[u8]){
+            let mut start_address = address;
+            for byte in value {
+                self.write(start_address, *byte);
+                start_address+=1;
+            }
+        }
 }
+
 
 impl Mos6502 {
     pub fn new(bus: Bus)->Mos6502{
@@ -61,9 +76,40 @@ impl Mos6502 {
          bus,
         } 
     }
+    pub fn reset(&mut self){
+        let address = ((VECTOR_BASE as u16) << 8) | RESET_VECTOR as u16;
+        let le = self.bus.read(address);
+        let hi = self.bus.read(address+1);
+        self.pc = Mos6502::get_address(hi, le);
+    }
+
+    fn get_address(hi:u8, le:u8)->u16{
+        ((le as u16) << 8) | (hi as u16)
+    }
 
     pub fn fetch(&mut self){
+        let opcode = self.bus.read(self.pc); 
+        println!("0x{:02X}", opcode);
+        let description = OPCODES_STRING[opcode as usize];
+        println!("{}", description);
+    let re = Regex::new(r"bytes:\s*(\d+)").unwrap();
 
+    if let Some(caps) = re.captures(description) {
+        if let Some(bytes_str) = caps.get(1) {
+            let bytes: u8 = bytes_str.as_str().parse().unwrap();
+            println!("Instruction length: {} bytes", bytes);
+            self.pc+= (bytes as u16);
+            // You can now use `bytes` to increment the program counter, etc.
+        } else {
+            println!("Failed to extract byte count.");
+        }
+    } else {
+        println!("Regex didn't match the description.");
+    }();
+    }
+
+    fn take_pc_byte(&mut self)->u8{
+        self.bus.read(self.pc)
     }
 }
 
@@ -116,5 +162,18 @@ impl Display for Bus {
             writeln!(f)?;
         }
         writeln!(f, "+-------------------------------------------+")
+    }
+}
+
+bitflags! {
+    #[derive(Debug)]
+    pub struct PFlag: u32 {
+        const Carry =            0b00000001;
+        const Zero =             0b00000010;
+        const InterruptDisable = 0b00000100;
+        const DecimalMode =      0b00001000;
+        const BreakCommand =     0b00010000;
+        const Overflow =         0b00100000;
+        const Negative =         0b01000000;
     }
 }

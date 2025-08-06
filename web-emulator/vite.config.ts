6b145b11d8e7575f618daa3d9d74b214523462e1
pgadula <gadula.przemyslaw94@gmail.com>
2025-08-06 13:47:42 +0200

    cleanup

--- web-emulator/vite.config.ts
+++ web-emulator/vite.config.ts
@@ -4,6 +4,6 @@ import topLevelAwait from 'vite-plugin-top-level-await'
 
 export default defineConfig({
   plugins: [wasm(), topLevelAwait()],
-  base: '/NES/', // <-- IMPORTANT
+  base: '/NES/',
 
 })
